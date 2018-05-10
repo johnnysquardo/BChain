68f1876769794fa7c1eaaa2977a673d1e4ecd417
John M Squardo <squ@rdo.host>
2018-05-09 21:34:02 -0500

    We ready...

--- bchain.py
+++ bchain.py
@@ -1,15 +1,19 @@
+# Initializing our blockchain list
 blockchain = []
 
 def get_last_blockchain_value():
+    """ Returns the last value of the current blockchain """
     return blockchain[-1]
 
 
 def add_value(transaction_amount, last_transaction=[1]):
+    """ Append a new value as well as the last blockchain value """
     blockchain.append([last_transaction, transaction_amount])
 
 
 def get_user_input():
-    return float(input('Your transaction amount please: '))
+    user_input = float(input('Your transaction amount please: '))
+    return user_input
     
 
 tx_amount = get_user_input()
@@ -21,4 +25,9 @@ add_value(last_transaction=get_last_blockchain_value(), transaction_amount=tx_am
 tx_amount = get_user_input()
 add_value(tx_amount, get_last_blockchain_value())
 
-print (blockchain)
+
+for block in blockchain: 
+    print('Outputting Block')
+    print(block)
+
+print('Done!')
