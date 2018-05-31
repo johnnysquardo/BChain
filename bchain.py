0ebb01448ebb92b5009ec178d2861a297665d368
Johnny Squardo <squ@rdo.host>
2018-05-31 16:53:12 -0400

    V0.1

--- bchain.py
+++ bchain.py
@@ -11,23 +11,32 @@ def add_value(transaction_amount, last_transaction=[1]):
     blockchain.append([last_transaction, transaction_amount])
 
 
-def get_user_input():
+def get_transaction_value():
     user_input = float(input('Your transaction amount please: '))
     return user_input
+
+def get_user_choice(): 
+    user_input = input('Your choise: ')
+    return user_input
+
+def print_blockchain_elements():
+    for block in blockchain: 
+        print('Outputting Block')
+        print(block)
     
 
-tx_amount = get_user_input()
+tx_amount = get_transaction_value()
 add_value(tx_amount)
 
-tx_amount = get_user_input()
-add_value(last_transaction=get_last_blockchain_value(), transaction_amount=tx_amount)
-
-tx_amount = get_user_input()
-add_value(tx_amount, get_last_blockchain_value())
-
-
-for block in blockchain: 
-    print('Outputting Block')
-    print(block)
+while True:
+    print('Please Choose')
+    print('1: Add a new transaction value')
+    print('2: Output the blockchain blocks')
+    user_choice = get_user_choice()
+    if user_choice == '1':
+        tx_amount = get_transaction_value()
+        add_value(tx_amount, get_last_blockchain_value())
+    else:
+        print_blockchain_elements()
 
 print('Done!')
