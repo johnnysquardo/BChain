68bdc36cb69d48466c635b988a51d4a3289fd6da
Johnny Squardo <squ@rdo.host>
2018-06-15 14:16:52 -0400

    Genesis Block Update

--- bchain.py
+++ bchain.py
@@ -1,7 +1,13 @@
 # Initializing our blockchain list
+genesis_block = {
+        'previous_hash': '',
+        'index': 0,
+        'transactions': []
+    }
 blockchain = []
 open_transactions = []
 owner = 'Johnny'
+
 
 # This gets the last value of the current blockchain
 def get_last_blockchain_value():
@@ -22,7 +28,13 @@ def add_transaction(recipient, sender=owner, amount=1.0):
     
 
 def mine_block(): 
-    pass
+    last_block = blockchain[-1]
+    block = {
+        'previous_hash': 'XYZ',
+        'index': len(blockchain),
+        'transactions': open_transactions
+    }
+    blockchain.append(block)
 
 # Gets the transaction value
 def get_transaction_value():
