53ab29df4ee391f553f06c3955b9351d0cfeb5ae
Johnny Squardo <squ@rdo.host>
2018-06-22 13:28:50 -0400

    Reduce function

--- bchain.py
+++ bchain.py
@@ -1,3 +1,5 @@
+import functools
+
 # Initializing our blockchain list
 
 MINING_REWARD = 10
@@ -21,15 +23,10 @@ def get_balance(participant):
     tx_sender = [[tx['amount'] for tx in block['transactions'] if tx['sender'] == participant] for block in blockchain]
     open_tx_sender = [tx['amount'] for tx in open_transactions if tx['sender'] == participant]
     tx_sender.append(open_tx_sender)
-    amount_sent = 0
-    for tx in tx_sender:
-        if len(tx) > 0:
-            amount_sent += tx[0]
+    amount_sent = functools.reduce(lambda tx_sum, tx_amt: tx_sum + tx_amt[0] if len(tx_amt) > 0 else 0, tx_sender, 0)
     tx_recipient = [[tx['amount'] for tx in block['transactions'] if tx['recipient'] == participant] for block in blockchain]
-    amount_recieved = 0
-    for tx in tx_recipient:
-        if len(tx) > 0:
-            amount_recieved += tx[0]
+    amount_recieved = functools.reduce(lambda tx_sum, tx_amt: tx_sum + tx_amt[0] if len(tx_amt) > 0 else 0, tx_recipient, 0)
+    #Return total balance
     return amount_recieved - amount_sent
 
 # This gets the last value of the current blockchain
