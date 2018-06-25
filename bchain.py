5cb8c17bbc362c1bef126234fdc446217f2e1635
Johnny Squardo <squ@rdo.host>
2018-06-25 12:53:59 -0400

    Sha256

--- bchain.py
+++ bchain.py
@@ -1,4 +1,6 @@
 import functools
+import hashlib
+import json
 
 # Initializing our blockchain list
 
@@ -16,7 +18,7 @@ owner = 'Johnny'
 participants= {'Johnny'}
 
 def hash_block(block): 
-    return '-'.join([str(block[key]) for key in block])
+    return hashlib.sha256(json.dumps(block).encode()).hexdigest()
 
 
 def get_balance(participant):
@@ -62,6 +64,7 @@ def add_transaction(recipient, sender=owner, amount=1.0):
 def mine_block(): 
     last_block = blockchain[-1]
     hashed_block = hash_block(last_block)
+    print(hashed_block)
     reward_transaction = {
         'sender': 'MINING',
         'recipient': owner,
