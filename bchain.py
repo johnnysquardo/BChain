52ff483ef6429caa42a6de726f1b5a13bd5df3fe
Johnny Squardo <squ@rdo.host>
2018-06-25 14:13:43 -0400

    PoW Functions

--- bchain.py
+++ bchain.py
@@ -17,8 +17,23 @@ owner = 'Johnny'
 
 participants= {'Johnny'}
 
+
 def hash_block(block): 
     return hashlib.sha256(json.dumps(block).encode()).hexdigest()
+
+def valid_proof(transactions, last_hash, proof):
+    guess = (str(transactions) + str(last_hash) + str(proof)).encode()
+    guess_hash = hashlib.sha256(guess).hexdigest()
+    print(guess_hash)
+    return guess_hash[0:2] == '00'
+
+def proof_of_work():
+    last_block = blockchain[-1]
+    last_hash = hash_block(last_block)
+    proof = 0
+    while valid_proof(open_transactions, last_hash, proof):
+        proof += 1
+    return proof
 
 
 def get_balance(participant):
