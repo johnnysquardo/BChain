b896f40350ae39295ab9679eda3b5806fcda4e56
Johnny Squardo <squ@rdo.host>
2018-07-02 08:43:26 -0400

    JsonPickle

--- bchain.py
+++ bchain.py
@@ -2,6 +2,7 @@ import functools
 import hashlib
 from collections import OrderedDict
 import json
+import pickle
 
 from hash_util import hash_string_256, hash_block
 
@@ -23,9 +24,13 @@ participants= {'Johnny'}
 
 def load_data():
     with open('blockchain.txt', mode='r') as f:
+        # file_content = pickle.loads(f.read())
         file_content = f.readlines()
+        
         global blockchain
         global open_transactions
+        # blockchain = file_content['chain']
+        # open_transactions = file_content['ot']
         blockchain = json.loads(file_content[0][:-1])
         updated_blockchain = []
         for block in blockchain:
@@ -56,6 +61,12 @@ def save_data():
         f.write(json.dumps(blockchain))
         f.write('\n')
         f.write(json.dumps(open_transactions))
+        # save_data = {
+        #     'chain': blockchain,
+        #     'ot': open_transactions
+        # }
+        # f.write(pickle.dumps(save_data))
+        
 
 
 def valid_proof(transactions, last_hash, proof):
