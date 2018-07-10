7a3044ea52f6f390130a8b28be4918a1468e284a
Johnny Squardo <squ@rdo.host>
2018-07-10 10:26:30 -0400

    Updating Classes

--- hash_util.py
+++ hash_util.py
@@ -4,5 +4,7 @@ import json
 def hash_string_256(string):
     return hashlib.sha256(string).hexdigest()
 
-def hash_block(block): 
-    return hash_string_256(json.dumps(block, sort_keys=True).encode())
+def hash_block(block):
+    hashable_block = block.__dict__.copy()
+    hashable_block['transactions'] = [tx.to_ordered_dict() for tx in hashable_block['transactions']]
+    return hash_string_256(json.dumps(hashable_block, sort_keys=True).encode())
